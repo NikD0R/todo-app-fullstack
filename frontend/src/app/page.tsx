ed7ae407541aac7d6614de4fc0cbbaad9c6d7a03
Mykyta Doroshenko <nikitokd05@gmail.com>
2026-07-22 02:28:54 +0300

    fix: disable ssr for TodoList component to fix vercel build

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,17 @@
-export const dynamic = "force-dynamic";
+import dynamic from "next/dynamic";
 
-import { TodoList } from "@/components/todo-list";
-import { Suspense } from "react";
+const TodoList = dynamic(
+  () => import("@/components/todo-list").then((mod) => mod.TodoList),
+  {
+    ssr: false,
+    loading: () => <div className="p-4">Loading tasks...</div>,
+  },
+);
 
 export default function Home() {
   return (
     <main className="max-w-3xl m-auto min-h-screen p-6 bg-slate-50 overflow-x-hidden w-full">
-      <Suspense fallback={<div>Loading tasks...</div>}>
-        <TodoList />
-      </Suspense>
+      <TodoList />
     </main>
   );
 }
